d5f8cf8a12829185b9209d989c05b58a82de7d22
xzcxvc <daryl.tiamps@gmail.com>
2024-10-10 15:03:56 +0800

    removed title

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <>
       <div className="bg-yellow-950 w-screen min-h-screen px-5">
         <div className="flex flex-col gap-[15px] min-h-screen justify-center items-center">
-          <h1 className="text-2xl font-bold">Blog Preview Card</h1>
           <div className="bg-white xs:w-[327px] xs:h-[501px] sm:w-[384px] sm:h-[525px] ring-1 ring-black rounded-[20px] shadow-solid">
             <div className="flex flex-col p-[24px] gap-[24px]">
               {/* article image */}
@@ -43,7 +42,7 @@ function App() {
               </div>
               {/* blogger */}
               <div className="flex gap-3 items-center ">
-                <img src={avatarImg} alt="user-image" width="32px" />
+                <img src={avatarImg} alt="user-image" width="32" />
                 <span className="font-extrabold">Greg Hooper</span>
               </div>
             </div>
